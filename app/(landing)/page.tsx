c2fd808ed16afbf1822918baa24a84e81b3ac631
wu649782027 <15151851516@163.com>
2024-01-02 13:46:50 +0800

    add meta

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,14 @@
 import LandingNavbar from "@/components/LandingNavbar";
 import LandingHero from "@/components/landing-hero";
 import LandingContent from "@/components/landing-content";
+import Head from "next/head";
 
 const LandingPage = () => {
   return (
     <div className={"h-full"}>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+      </Head>
       <LandingNavbar/>
       <LandingHero/>
       <LandingContent/>
